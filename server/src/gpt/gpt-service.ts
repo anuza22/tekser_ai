c60b7a72dc1e778cd8feb1f3599d3ca358e80972
Anuza <a_akturina@kbtu.kz>
2024-06-27 00:12:34 +0500

    n!

--- server/src/gpt/gpt-service.ts
+++ server/src/gpt/gpt-service.ts
@@ -1,82 +1,5 @@
-// import OpenAI from 'openai';
-// import { systemPrompt } from './prompt'; // Ensure you have the system prompt defined here
-// import { Mark } from './models/types';
-// import * as fs from 'fs';
-// import { ChatCompletionContentPart } from 'openai/resources/chat/completions';
-
-// const openai = new OpenAI({
-//   apiKey: process.env.OPENAI_API_KEY,
-// });
-
-// class GPTservice {
-//   async checkHW(imagePaths: string[]) {
-//     console.log(imagePaths);
-//     try {
-
-//       if (imagePaths.length >0) {
-//         const response = await openai.chat.completions.create({
-//           model: "gpt-4o",
-//           messages: [
-//       {
-//         role: "user",
-//         content: [
-//           { type: "text", text: systemPrompt },
-//           {
-//             type: "image_url",
-//             image_url: {
-//               "url":imagePaths[0],
-//             },
-//           },
-//           {
-//             type: "image_url",
-//             image_url: {
-//               "url":imagePaths[1],
-//             },
-//           },
-//           {
-//             type: "image_url",
-//             image_url: {
-//               "url":imagePaths[2],
-//             },
-//           },
-//           {
-//             type: "image_url",
-//             image_url: {
-//               "url":imagePaths[3],
-//             },
-//           },
-//           {
-//             type: "image_url",
-//             image_url: {
-//               "url":imagePaths[4],
-//             },
-//           },
-//         ],
-//       },
-//     ],
-//         });
-        
-//         console.log(response.choices);
-//         const resContent = response.choices[0].message.content;
-
-//         if (resContent) {
-//           const parsedRes = JSON.parse(resContent);
-//           return parsedRes.mark as Mark[];
-//         } else {
-//           return null;
-//         }
-//       }
-//     } catch (error: any) {
-//       console.log(error.message);
-//       return null;
-//     }
-//   }
-// }
-
-// export default GPTservice;
-
 import OpenAI from 'openai';
-import { systemPromptForMath, systemPromptForPhysic } from './prompt'; // Ensure you have the system prompt defined here
+import { systemPrompt } from './prompt';
 import { Mark } from './models/types';
 
 const openai = new OpenAI({
@@ -86,7 +9,7 @@ const openai = new OpenAI({
 const DEFAULT_IMAGE_URL = "https://img.freepik.com/premium-photo/watercolor-paper-texture-background-with-clipping-path-white-paper-sheet-with-torn-edges-isolated-gray-art-paper-high-quality-texture-high-resolution_64749-3881.jpg";
 
 class GPTservice {
-  async checkHW(imagePaths: string[]) {
+  async checkHW(imagePaths: string[], subject: string, grade: number, language: string, textbook: string) {
     console.log(imagePaths);
     try {
       // Ensure there are exactly 5 image URLs
@@ -95,36 +18,39 @@ class GPTservice {
         filledImagePaths.push(DEFAULT_IMAGE_URL);
       }
 
-      const response = await openai.chat.completions.create({
-        model: "gpt-4o",
-        messages: [
-          {
-            role: "user",
-            content: [
-              { type: "text", text: systemPromptForMath },
-              {
-                type: "image_url",
-                image_url: { "url": filledImagePaths[0] },
-              },
-              {
-                type: "image_url",
-                image_url: { "url": filledImagePaths[1] },
-              },
-              {
-                type: "image_url",
-                image_url: { "url": filledImagePaths[2] },
-              },
-              {
-                type: "image_url",
-                image_url: { "url": filledImagePaths[3] },
-              },
-              {
-                type: "image_url",
-                image_url: { "url": filledImagePaths[4] },
-              },
-            ],
-          },
-        ],
+      const prompt = systemPrompt
+        .replace('{subject}', subject)
+        .replace('{language}', language)
+        .replace('{textbook}', textbook)
+        .replace('{grade}', `${grade}`);
+
+      const response = await openai.chat.completions.create ({
+        model: "gpt-4",
+        messages: [{
+                  role: "user",
+                  content: [
+                    { type: "text", text: prompt },
+                    {
+                      type: "image_url",
+                      image_url: { "url": filledImagePaths[0] },
+                    },
+                    {
+                      type: "image_url",
+                      image_url: { "url": filledImagePaths[1] },
+                    },
+                    {
+                      type: "image_url",
+                      image_url: { "url": filledImagePaths[2] },
+                    },
+                    {
+                      type: "image_url",
+                      image_url: { "url": filledImagePaths[3] },
+                    },
+                    {
+                      type: "image_url",
+                      image_url: { "url": filledImagePaths[4] },
+                    }
+        ],},]
       });
 
       console.log(response.choices);
@@ -132,7 +58,7 @@ class GPTservice {
 
       if (resContent) {
         const parsedRes = JSON.parse(resContent);
-        return parsedRes.mark as Mark[];
+        return parsedRes.mark as Mark;
       } else {
         return null;
       }
--- server/src/gpt/prompt.ts
+++ server/src/gpt/prompt.ts
@@ -1,43 +1,9 @@
-export const systemPromptForMath = `
-Вы - учитель математики с многолетним опытом преподавания. 
-Все ученики любят вас, потому что вы хорошо объясняете тему, даете дельные советы по выполнению домашних заданий. 
-Вы учились в лучшем техническом университете мира. И теперь вы преподаете с 5 по 12 класс. 
-#### Теперь вам нужно проверять домашние задания по математике у своих учеников. В соответствии со следующими критериями:
-1. Логика и метод решения задач
-2. Игнорировать тот факт, что записи на рабочем листе могут быть хаотичными, т.е. не последовательными.
-3. Рассмотрите различные методы решения
-и поставьте оценку домашнему заданию от 0 до 10. Дайте обратную связь студенту, какие задачи он решил правильно, 
-а в каких ошибся и что ему следует исправить, какие темы повторить, чтобы улучшить свои знания и не повторять ошибок, как в этот раз.
+export const systemPrompt = `
+You are a {subject} teacher with many years of teaching experience.
+All students love you because you explain the topic well, give good advice on how to do their homework.
+You went to the best technical university in the world. And now you're teaching grades {grade}.
 
-Так же напиши мне запрос для поисковика в GOOGLE для поиска сайтов где можно получить знания по тем темам где ошибся ученик.
-
-Please, return your response in following JSON format: 
-[
-      { "mark": 5,
-       "correct problems": "number 1 problem was correct, deals with topics such as Quadratic Equations",
-       "wrong tasks": ""
-       "feedback": "You're an innovative and creative person. It's very 
-        clear to me that you enjoy solving problems in different ways and 
-        using your creativity to develop new ideas. It's great seeing you 
-        use your creativity and innovation to perform well in school.",
-      "mistakes": "Be sure you write your answers down correctly: I see this a lot, 
-       and one small mistake, such as dropping a negative, can have drastic changes. You should do like sqrt(9)",
-       "searchPrompt": "Как решать квадратичные уравнения?"
-}
-  ]
-
-If user prompt is irrelevant return empty json of mark
-`;
-
-export const userPrompt = 'https://thumbs.dreamstime.com/b/handwritten-math-text-grouped-isolated-white-vector-illustration-quadratic-equation-formula-decision-scheme-trigonometric-188860394.jpg';
-
-
-export const systemPromptForPhysic = `
-You are a physics teacher with many years of teaching experience. 
-All students love you because you explain the topic well, give good advice on how to do their homework. 
-You went to the best technical university in the world. And now you're teaching grades 5 through 12. 
-
-#### Now you need to check your students' physics homework. According to the following criteria:
+#### Now you need to check your students' {subject} homework. According to the following criteria:
 1. Logic and method of problem solving
 2. Ignore the fact that the entries on the worksheet may be chaotic, i.e., not sequential.
 3. Consider different methods of solution
@@ -56,4 +22,7 @@ Please, return your response in the following JSON format:
 ]
 
 If user prompt is irrelevant return an empty JSON of mark.
+
+Response language: {language}
+Textbook: {textbook}
 `;
